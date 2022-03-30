64754764054e6f059f5d664a97c1f361e8266140
ricardo <ricardo.baumann@krieger.de>
2022-03-30 13:26:32 +0200

    deposit use case

--- src/main/java/com/github/ricbau/vendingmachine/api/controllers/mappers/DepositResultMapper.java
+++ src/main/java/com/github/ricbau/vendingmachine/api/controllers/mappers/DepositResultMapper.java
@@ -2,7 +2,9 @@ package com.github.ricbau.vendingmachine.api.controllers.mappers;
 
 import com.github.ricbau.vendingmachine.api.controllers.results.CreateDepositResult;
 import com.github.ricbau.vendingmachine.domain.entities.Deposit;
+import com.github.ricbau.vendingmachine.domain.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -10,13 +12,14 @@ import java.net.URI;
 @SuppressWarnings("unused")
 @Mapper(componentModel = "spring")
 public interface DepositResultMapper {
+    @Mapping(target = "userLocation", source = "affectedUser")
     CreateDepositResult toResult(Deposit deposit);
 
-    default URI toURI(String userId) {
+    default URI toURI(User user) {
         return ServletUriComponentsBuilder
                 .fromCurrentContextPath()
                 .path("/users/{userId}")
-                .buildAndExpand(userId)
+                .buildAndExpand(user.getId())
                 .toUri();
     }
 }
